069dbe3f6da628eef673a3b56abcde4a546879b4
Andre Cabrera <andre@andrecabrera.com>
2021-03-23 16:25:37 -0300

    Fix build script

---
+++ build/precache-manifest.213710fc84b5ef8bf2dc0ca279bf83dc.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4004cbd886b91a4d6e5a088f11c2f041",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a0eeb2b891a3a8c191d",
+    "url": "/static/css/2.c5b04194.chunk.css"
+  },
+  {
+    "revision": "d512b06fe376dbc7de08",
+    "url": "/static/css/main.1a3b7563.chunk.css"
+  },
+  {
+    "revision": "0a0eeb2b891a3a8c191d",
+    "url": "/static/js/2.a5bf0ae0.chunk.js"
+  },
+  {
+    "revision": "d512b06fe376dbc7de08",
+    "url": "/static/js/main.eb76ebe3.chunk.js"
+  },
+  {
+    "revision": "222457504c44045c75f9",
+    "url": "/static/js/runtime-main.af08e69b.js"
+  },
+  {
+    "revision": "cb0541dc8c4730ea976f630c90286f86",
+    "url": "/static/media/12-bitcoin-cash-square-crop.2f0cb496.svg"
+  },
+  {
+    "revision": "078ba7a3ea3b6469b4ec87b46ec495ae",
+    "url": "/static/media/antd.1fb1fa95.less"
+  },
+  {
+    "revision": "2c0f62116449ad3be30bcf8ec8185a91",
+    "url": "/static/media/edit.571e35c6.svg"
+  },
+  {
+    "revision": "dfe5aa4344a2d8a29aec8d83fb3fb14e",
+    "url": "/static/media/fa-brands-400.330e879a.ttf"
+  },
+  {
+    "revision": "0ce1e868452204695c8ac1c70f693c2d",
+    "url": "/static/media/fa-brands-400.5f63cb7f.woff"
+  },
+  {
+    "revision": "0c9f225e8f69c622f681cf1ed973cc3d",
+    "url": "/static/media/fa-brands-400.6e63bd22.woff2"
+  },
+  {
+    "revision": "d28c96cabe5302f1b2c90275cd3f4e69",
+    "url": "/static/media/fa-brands-400.98f20b9e.eot"
+  },
+  {
+    "revision": "5d10d00980077f67990f73b1961bc46e",
+    "url": "/static/media/fa-brands-400.991c1c76.svg"
+  },
+  {
+    "revision": "847712aaabbeba674afdda86d31cab17",
+    "url": "/static/media/fa-regular-400.2c154b0f.woff2"
+  },
+  {
+    "revision": "0232fe8b09de812c88e6cc27f57a3a88",
+    "url": "/static/media/fa-regular-400.62a07ffe.eot"
+  },
+  {
+    "revision": "398be22b2308585b84da8e858e2d7b60",
+    "url": "/static/media/fa-regular-400.ac236764.ttf"
+  },
+  {
+    "revision": "3cace4a04d941b5981ba32c6ce9afae1",
+    "url": "/static/media/fa-regular-400.ea5a41ec.woff"
+  },
+  {
+    "revision": "d0fdc7c20b0b0b1aaf27327547735fa3",
+    "url": "/static/media/fa-regular-400.f3187c74.svg"
+  },
+  {
+    "revision": "2306f642b31b774ba1d8b6b784539e19",
+    "url": "/static/media/fa-solid-900.0454203f.svg"
+  },
+  {
+    "revision": "9ae050d1876ac1763eb6afe4264e6d5a",
+    "url": "/static/media/fa-solid-900.3eb06c70.woff2"
+  },
+  {
+    "revision": "00bb62bc33034479d5e82a0fd1c5efe3",
+    "url": "/static/media/fa-solid-900.6606667d.eot"
+  },
+  {
+    "revision": "260d033f79f75d670fd09d8cf7c8bf8d",
+    "url": "/static/media/fa-solid-900.915a0b79.ttf"
+  },
+  {
+    "revision": "4bfbf7eb4b19d9ff9293eb177b6d0070",
+    "url": "/static/media/fa-solid-900.f4f93856.woff"
+  },
+  {
+    "revision": "bccb63203ce9ca88edb2230fe8e713c1",
+    "url": "/static/media/trashcan.3366d410.svg"
+  }
+]);
--- build/serviceWorker.js
+++ build/serviceWorker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.e3361805abb4f28ab567164dd1a49aa2.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.213710fc84b5ef8bf2dc0ca279bf83dc.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.setConfig({
     debug: false,
