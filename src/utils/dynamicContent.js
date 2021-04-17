ed190c14101440fa4ac7dfa9e12377d877bc7736
Andre Cabrera <andre@andrecabrera.com>
2021-04-17 07:12:42 -0300

    Add Dynamic Content utils

---
+++ src/utils/dynamicContent.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+const AIRTABLE_READONLY_API_KEY = "keyc9Awfjh1RPAbyZ";
+const endpointMap = {
+    market: "https://api.airtable.com/v0/appYoHNjKSpv1cPF2/Assets%20Submissions?maxRecords=1&sort%5B0%5D%5Bfield%5D=ID&sort%5B0%5D%5Bdirection%5D=desc&view=Grid%20view",
+    news: "https://api.airtable.com/v0/appYoHNjKSpv1cPF2/News?maxRecords=3&sort%5B0%5D%5Bfield%5D=ID&sort%5B0%5D%5Bdirection%5D=desc&view=Grid%20view",
+}
+
+const dynamicContent = async (endpoint) => {
+            //TODO: Move to environment variables
+            const responseFromAirtable = await axios.get(endpointMap[endpoint], {
+                headers: {
+                    "Authorization": `Bearer ${airtableReadOnlyApiKey}`
+                }
+            })
+
+            return responseFromAirtable;
+}
