7bc45280fe286aee6a7f84cedb6396115e751fbe
Andre Cabrera <git@andrecabrera.com>
2021-06-18 05:11:38 -0300

    fix: reveal seed

--- src/components/Common/PassCheck.js
+++ src/components/Common/PassCheck.js
@@ -1,55 +1,45 @@
-import React, {} from 'react'
+import React from "react";
 import { WalletContext } from "@utils/context";
-import FormPassword from '@components/OnBoarding/formPassword'
-
+import FormPassword from "@components/OnBoarding/formPassword";
 
 export default function PassCheck(props) {
-    const advice = props.advice;
-    return (
-        <>
-        {/* <FormPassword
-            getWallet={() => wallet}
-            textSubmit="Reveal Seed"
-        >
+  const ContextValue = React.useContext(WalletContext);
+  const { wallet } = ContextValue;
+  const advice = props.advice;
+  return (
+    <>
+      <FormPassword getWallet={() => wallet} textSubmit="Reveal Seed">
         <p>{wallet && wallet.mnemonic ? wallet.mnemonic : ""}</p>
-		</FormPassword> */}
-        <div style={span_style}>
-        {advice}
-        </div>
-        <div style={comp_style}>
-
-            <input style={input_style} placeholder='Password' type="password"/>
-            <button style={button_style}>Send</button>
-        </ div>
-        </>
-    )
+      </FormPassword>
+      <div style={span_style}>{advice}</div>
+    </>
+  );
 }
-
 
 const comp_style = {
-    display: 'flex',
-    justifyContent: 'center',
-    margin: 'auto 20px',
-    padding:'10px'
-}
+  display: "flex",
+  justifyContent: "center",
+  margin: "auto 20px",
+  padding: "10px",
+};
 const button_style = {
-    border: 'solid 2px #DC1199',
-    backgroundColor: '#DC1199',
-    padding: '6px 14px',
-    borderRadius: '6px',
-    color: 'white',
-    margin: '10px'
-}
+  border: "solid 2px #DC1199",
+  backgroundColor: "#DC1199",
+  padding: "6px 14px",
+  borderRadius: "6px",
+  color: "white",
+  margin: "10px",
+};
 const input_style = {
-    border: 'solid 2px #DC1199',
-    backgroundColor: '#fff',
-    padding: '6px',
-    borderRadius: '6px',
-    margin: '10px'
-}
+  border: "solid 2px #DC1199",
+  backgroundColor: "#fff",
+  padding: "6px",
+  borderRadius: "6px",
+  margin: "10px",
+};
 const span_style = {
-    textAlign: 'center',
-    color: '#011437',
-    fontSize: '12px',
-    marginTop:'14px'
-}
+  textAlign: "center",
+  color: "#011437",
+  fontSize: "12px",
+  marginTop: "14px",
+};
