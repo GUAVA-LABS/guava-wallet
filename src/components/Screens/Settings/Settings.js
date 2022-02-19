2d40e7bb603b4723ef0e0f64071b0937358c979a
dostoievsky <bernardo.emery@gmail.com>
2022-02-19 12:06:15 -0300

    refactor: moved formPassword responsabilites to its own folder, and made it a named export

--- src/components/Screens/Settings/Settings.js
+++ src/components/Screens/Settings/Settings.js
@@ -3,20 +3,20 @@ import { WalletContext } from "@utils/context";
 
 import InfoBar from '@components/Common/InfoBar';
 
+import { FormPassword } from '../../OnBoarding/formPassword';
+
 import DeleteModal from './DeleteModal';
 
 import './Settings.css';
 
 const Settings = () => {
     const ContextValue = React.useContext(WalletContext);
-    const { wallet, deleteWallet, decryptData } = ContextValue;
+    const { deleteWallet } = ContextValue;
   
     const[open, setOpen] = useState({
       open: false
     });
 
-    const [backup, setBackup] = useState(false);
-    const [password, setPassword] = useState('');
     const [mnemonic, setMnemonic] = useState(false);
     const [decpryt, setDecrypt] = useState(false);
 
@@ -24,46 +24,22 @@ const Settings = () => {
       setOpen({open: !open.open});
     }, [open.open]);
 
-    const handlePasswordChange = useCallback((e) => {
-      setPassword(e.target.value);
-    }, []);
-
-  const handleDecrypt = useCallback(async (e) => {
-    e.preventDefault();
-    const data = decryptData(password, wallet.mnemonicCypher);
-    if (data) {
-      setDecrypt(true);
-      setMnemonic(data);
-    }
-    setPassword('');
-  }, [decryptData, password, wallet.mnemonicCypher]);
-
     return (
         <div className='container'>
             <InfoBar 
               title='Backup your wallet'
               droppable='true' 
               content={
-                  <div>
+                  <>
                     {decpryt ? 
                       mnemonic : 
-                      <form onSubmit={handleDecrypt}>
-                        <input 
-                          name='password' 
-                          className='confirm-input' 
-                          placeholder='Password' 
-                          type='password'
-                          onChange={(e) => handlePasswordChange(e)}
-                        />
-                      </form>
+                      <FormPassword 
+                        setMnemonic={setMnemonic}
+                        setDecrypt={setDecrypt}
+                      />
                     }
-                  </div>
+                  </>
               }/>
-               
-
-        
-         
-          
 
           <InfoBar
             title='Documentation'
