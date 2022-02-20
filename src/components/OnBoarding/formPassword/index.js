19d1ad243c6d5bb0df389ba34813f734df40ddf3
Bernardo Emery <bernardo.emery@gmail.com>
2022-02-20 19:49:18 -0300

    Feature: password errors (#23)
    * style: added style to password label
    
    * style: added border, placeholder font style and some visual to password input
    
    * style: added Error container
    
    * style: added a margin to button from the end of div
    
    * feat: added Error container, state and it HTML position on code
    
    * feat: added Errors bussiness rules
    
    * feat: added input label and deleted unused css class

--- src/components/OnBoarding/formPassword/index.js
+++ src/components/OnBoarding/formPassword/index.js
@@ -4,15 +4,16 @@ import {FiEyeOff, FiEye} from 'react-icons/fi';
 
 import { WalletContext } from "@utils/context";
 
-import { Container, Submit } from './styles'
+import { Container, Error , Submit } from './styles'
 
 export const FormPassword = ({setMnemonic, setDecrypt}) => {
   const ContextValue = React.useContext(WalletContext);
   const { wallet, decryptData } = ContextValue;
 
   const [password, setPassword] = useState('');
-
   const [passwordView, setPasswordView] = useState(false);
+
+  const [error, setError] = useState('');       
   
   const handlePasswordChange = useCallback((e) => {
     setPassword(e.target.value);
@@ -20,21 +21,27 @@ export const FormPassword = ({setMnemonic, setDecrypt}) => {
 
   const handleDecrypt = useCallback((e) => {
     e.preventDefault();
-    const data = decryptData(password, wallet.mnemonicCypher);
-    if (data) {
-      setDecrypt(true);
-      setMnemonic(data);
+    setError('');
+    if (password.length > 6) {
+      const data = decryptData(password, wallet.mnemonicCypher);
+      if (data) {
+        setDecrypt(true);
+        setMnemonic(data);
+        setPassword('');
+      }else{
+        setError('Invalid Password, please try again');
+      }
+    }else{
+      setError('Password must have more than 6 characters');
     }
-    setPassword('');
   }, [decryptData, password, setDecrypt, setMnemonic, wallet.mnemonicCypher]);
 
   return (
     <Container>
       <form onSubmit={handleDecrypt}>
-
+        <h1>Insert your password: </h1>
         <input 
           name='password' 
-          className='confirm-input' 
           placeholder='Password' 
           type={!passwordView ? 'password' : 'text'}
           onChange={(e) => handlePasswordChange(e)}
@@ -46,6 +53,12 @@ export const FormPassword = ({setMnemonic, setDecrypt}) => {
         >
         {!passwordView ? <FiEyeOff/> : <FiEye/>}
         </button>
+
+        <Error>
+          <span>
+            {error}
+          </span>
+        </Error>
         
         <Submit>
           <button 
--- src/components/OnBoarding/formPassword/styles.js
+++ src/components/OnBoarding/formPassword/styles.js
@@ -4,14 +4,36 @@ export const Container = styled.div`
     width: 100%;
     align-items: center;
     form {
-        input {
-            width: 160px;
-            height: 8px;
+
+        h1 {
+            font-size: 14px;
         }
+    
+        input {
+            flex: 1;
+            background: transparent;
+            border-radius: 6px;            
+            width: 160px;
+            height: 6px;
+            padding: 12px;
+
+            &::placeholder {
+                color: #000;
+            }
+        }
+    }
+`;
+
+export const Error = styled.div`
+    span {        
+        color:  #c53030;
+        margin-bottom: 8px;
+        font-size: 12px;
     }
 `;
 
 export const Submit = styled.div`
     margin: 0 auto;
     display: block;
+    margin-bottom: 8px;
 `
