ecb90f44cfb1110477f7d2b7fddff74738e35562
Bernardo Emery <bernardo.emery@gmail.com>
2022-03-20 20:55:06 -0300

    feat: working USD balance (#34)

--- src/components/Common/Header/Header.js
+++ src/components/Common/Header/Header.js
@@ -1,83 +1,84 @@
-import React, { useState, useEffect} from 'react'
-import '../../../global.css'
-import './Header.css'
-import logoPinkBg from '../../../assets/guavaheader.svg'
-// import { Modal } from "@components/Common/Modal/modal"
-import { List } from "@components/Common/List/List"
-import sendIcon from '../../../assets/send-icon.png'
-import receiveIcon from '../../../assets/receive-icon.png'
-import Transactions from '@components/Common/Transactions/Transactions'
-import { currency } from '@components/Common/Ticker'
+import React, { useState, useEffect, useCallback} from 'react';
+import logoPinkBg from '../../../assets/guavaheader.svg';
+import sendIcon from '../../../assets/send-icon.png';
+import receiveIcon from '../../../assets/receive-icon.png';
+import Transactions from '@components/Common/Transactions/Transactions';
 
-export default function Header(props) {
+import axios from 'axios';
+
+import '../../../global.css';
+import './Header.css';
+
+export const Header = (props)  => {
+
+    const [balanceUSD, setBalanceUSD] = useState();
+
+    const AvaxConverter = axios.create({
+        baseURL: 'https://api.coingecko.com/api/'
+    })
 
     const[open, setOpen] = useState({
         open: false,
         action: ''
     });
 
-    useEffect(() => {
-    }, [open])
-
-    function handleOpen(action) {
-        // if(action == '')
+    const handleOpen = useCallback((action) => {
         setOpen({open:!open.open, action: action})
-        // getAvaxPrice()
-        // setOpen(!open)
-        // setAction(action)
-        // setOpen((prev) => {
-        //     return({...prev, open:!prev.open, action: action})
-        // })
-    };
+    }, [open.open]);
     
-    async function getAvaxPrice(){
-        const url = 'https://avascan.info/api/v2/price'
-        const response = await fetch(url, {
-            mode: 'no-cors',
+    const getAvaxPrice = useCallback(async() => {
+
+        await AvaxConverter.get('v3/simple/price?ids=avalanche-2&vs_currencies=usd', {
             headers: {
-                "Access-Control-Allow-Origin" : "*", 
-                "Access-Control-Allow-Credentials" : true 
-            }
-        });
-        const data = await response.json()
-        console.log(data)
-    }
+                'Access-Control-Allow-Origin': '*',
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+	    }).then((response) => {
+            const data =  Object.values(response.data);
+            setBalanceUSD(Number(data[0].usd) * Number(props.avaxBalance))
+	    }).catch((e) => {
+		    console.log(e, 'error');
+	    });
+
+    }, [AvaxConverter, props.avaxBalance]);
+
+    useEffect(() => {
+        getAvaxPrice(); 
+    }, [getAvaxPrice])
 
     return (
         <>
             <div className='box-complete bg-pink-1 header'>
 
                     <div className='right_content'>
-                        <img className='logo' src={logoPinkBg}/>
+                        <img className='logo' alt='logo' src={logoPinkBg}/>
                         <span className='version'>
                         Version 2.0
                         </span>
                     </div>
                     <div className='info'>
                         <h1 className='balance-usd'>
-                            {props.balanceUSD}
-                            {
-                                
-                            }
+                            {`${balanceUSD} USD`}
                         </h1>
                         <h3 style={{margin:'0px !important'}} className='balance-avax'>
                             {props.balanceAVAX}
                         </h3>
                     </div>
                     <div className='transactions'>
-                        <a id='btn-receive' className='transaction-button btn-send'
+                        <div id='btn-receive' className='transaction-button btn-send'
                             onClick={() => handleOpen('send')}
                             >
-                            <img className='transIcon' src={sendIcon} />
+                            <img className='transIcon' alt='Send Avax' src={sendIcon} />
                             SEND
-                        </a>
+                        </div>
                         <div className='vline'></div>
-                        <a id='btn-send' className='transaction-button btn-receive'
+                        <div id='btn-send' className='transaction-button btn-receive'
                             onClick={() => handleOpen('receive')}
                             >
-                            <img className='transIcon' src={receiveIcon} />
+                            <img className='transIcon' alt='Receive Avax' src={receiveIcon} />
                             RECEIVE
-                        </a>
+                        </div>
                     </div>
                     {
                         open.open ? <Transactions open={open.open} action={open.action} setOpen={setOpen} handleOpen={handleOpen}/> : null
